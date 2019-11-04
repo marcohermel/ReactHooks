c12701a68c402e757c104d72e45d29917d17b8da
Marco <mf.hermel@gmail.com>
2019-11-04 08:45:08 -0300

    Updates

---
+++ precache-manifest.c228587f4273f6aec6f1d6430dfa7b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d729dafb1aef7885687196eee3a35d",
+    "url": "/ReactHooks/index.html"
+  },
+  {
+    "revision": "1c6129f65e3c139c2740",
+    "url": "/ReactHooks/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4af6a066c4d2611627b7",
+    "url": "/ReactHooks/static/css/main.a57d034c.chunk.css"
+  },
+  {
+    "revision": "1c6129f65e3c139c2740",
+    "url": "/ReactHooks/static/js/2.a93c6823.chunk.js"
+  },
+  {
+    "revision": "4af6a066c4d2611627b7",
+    "url": "/ReactHooks/static/js/main.ee5dd5fa.chunk.js"
+  },
+  {
+    "revision": "a574d43d68c8bbe6384a",
+    "url": "/ReactHooks/static/js/runtime~main.1079bf91.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff2becb2f059b0479bea759a51dbbdb1.js"
+  "/ReactHooks/precache-manifest.c228587f4273f6aec6f1d6430dfa7b9d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactHooks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
